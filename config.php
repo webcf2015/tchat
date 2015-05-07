2250ae00a3db7643823be677961537085aa85fd4
Pitz Michaël <michaeljpitz@gmail.com>
2015-05-07 16:07:36 +0200

    Mise à jour DB

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
-define("DBHOST", "localhost");
-define("DBLOGIN", "root");
-define("DBMDP", "");
-define("DBNAME", "tchat");
+define("DBHOST", "cl1-sql15");
+define("DBLOGIN", "dessycf2m12");
+define("DBMDP", "SDwcZnzfqrVG");
+define("DBNAME", "dessycf2m12");
 //echo $chemin_absolu = $_SERVER['HTTP_HOST'];
 
 // chemin absolu vers le projet en cours
-define("PATH","//localhost/ajax/tchat/V0.1");
+define("PATH","//tchat.moncf2m.be");
 
 // nb de messages affichés dans le tchat
 $nb_messages_tchat = 30;
